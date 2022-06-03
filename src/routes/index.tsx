ab242388a71034827b68545b2c48b6584ae110d7
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-06-03 14:14:36 -0300

    feat: use session to define initial router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '../@types/routes';
 
+import useSession from '../hooks/useSession';
+
 import Welcome from '../screens/Welcome';
 import SignIn from '../screens/SignIn';
 import SignUp from '../screens/SignUp';
 
+import App from './app';
+
+
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export default function Routes() {
+  const session = useSession();
+
   return (
-    <Navigator>
+    <Navigator initialRouteName={session.account ? 'App' : 'Welcome'}>
       <Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
       <Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
       <Screen name='SignUp' component={SignUp} />
+      <Screen name="App" component={App} options={{ headerShown: false }} />
     </Navigator>
   )
 }
