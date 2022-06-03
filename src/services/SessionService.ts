4fe84a465cd466a3694c0413c66c6e275b907b62
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-06-03 14:11:58 -0300

    feat: Session Service

---
+++ src/services/SessionService.ts
@@ -0,0 +1,14 @@
+import { Session } from "../schemas/session";
+import api from "./api"
+
+export default {
+  login: async (email: string, password: string) => {
+    const response = await api.post<Session | Error>('/session', { email, password });
+
+    if (response.status === 200) {
+      return response.data as Session;
+    }
+
+    throw new Error((response.data as Error).message);
+  }
+}
