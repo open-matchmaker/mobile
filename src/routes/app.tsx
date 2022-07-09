8c3d83e7577911833efe7723a34967c1729fd841
Amarakk <arthuramaral.souza@gmail.com>
2022-07-09 17:41:59 -0300

    Fix na navigation

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -6,7 +6,6 @@ import AppContextProvider from '../contexts/app';
 import Home from './home';
 import Post from '../screens/Post';
 import Profile from '../screens/Profile';
-import Queue from '../screens/Queue';
 
 const { Navigator, Screen } = createNativeStackNavigator<AppStackParamList>();
 
@@ -16,8 +15,7 @@ export default function Routes() {
       <Navigator initialRouteName='Home'>
         <Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Screen name="Post" component={Post} />
-        <Screen name="Profile" component={Profile} />
-        <Screen name="Queue" component={Queue} />
+        <Screen name="Profile" component={Profile} />   
       </Navigator>
     </AppContextProvider>
   )
